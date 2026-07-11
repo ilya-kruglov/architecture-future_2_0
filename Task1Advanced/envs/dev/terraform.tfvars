0c331fb2a7522ed6855ad0ed2f0ce060aeb332fb
banbao <ilya.kruglove@yandex.ru>
2026-07-11 16:02:43 +0300

    feat(Task1Advanced): add reusable Terraform module for VM with multi-environment support

---
+++ Task1Advanced/envs/dev/terraform.tfvars
@@ -0,0 +1,5 @@
+cloud_id       = "your-cloud-id"
+folder_id      = "your-folder-id"
+token          = "your-oauth-token"
+subnet_id      = "e9b8m9t9p9k9a9b9c9d9"  # реальный ID подсети для dev
+ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQ..." # ваш публичный ключ
---
+++ Task1Advanced/envs/stage/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "~> 0.78"
+    }
+  }
+}
+
+variable "cloud_id" {
+  description = "ID облака Yandex Cloud"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "ID каталога Yandex Cloud"
+  type        = string
+}
+
+variable "token" {
+  description = "OAuth-токен для доступа к Yandex Cloud"
+  type        = string
+  sensitive   = true
+}
+
+variable "subnet_id" {
+  description = "ID подсети для ВМ"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "Публичный SSH-ключ"
+  type        = string
+  sensitive   = true
+}
+
+provider "yandex" {
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  token     = var.token
+}
+
+module "vm" {
+  source = "../../modules/vm"
+
+  instance_name   = "stage-vm"
+  zone            = "ru-central1-b"
+  cores           = 4
+  ram             = 8
+  subnet_id       = var.subnet_id
+  ssh_public_key  = var.ssh_public_key
+  disk_size       = 50
+  disk_type       = "network-ssd"
+  boot_disk_size  = 20
+  boot_disk_type  = "network-ssd"
+}
